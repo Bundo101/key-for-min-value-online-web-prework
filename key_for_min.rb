53af64272aa7547b82728b51240c3096a85bd1f7
James Britton <brittonjp@gmail.com>
2019-05-02 14:59:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     min_val = nil
     min_key = nil
     name_hash.each do |k, v|
-      if v < min_val
+      if min_val == nil || v < min_val
         min_val = v
         min_key = k
       end
